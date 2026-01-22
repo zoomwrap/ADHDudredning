66e0aedd54e32121b4dc3db8df6a7cbcab23808b
Kim Lasse Massesson <kima4@elev.tec.dk>
2026-01-22 22:35:33 +0100

    meget Nyt

--- script.js
+++ script.js
@@ -5,21 +5,19 @@ fetch("da.json")
     document.getElementById("navlink1").textContent = data.navlink1;
     document.getElementById("navlink2").textContent = data.navlink2;
     document.getElementById("navlink3").textContent = data.navlink3;
+        document.getElementById("navlink4").textContent = data.navlink4;
+        document.getElementById("navlink5").textContent = data.navlink5;
+        document.getElementById("navlink6").textContent = data.navlink6;
+        document.getElementById("navlink7").textContent = data.navlink7;
     document.getElementById("heroImage").style.backgroundImage =
       `url('${data.heroImage.src}')`;
     document.getElementById("heroImage").alt = data.heroImage.alt;
     document.getElementById("title").textContent = data.title;
     document.getElementById("subtitle").textContent = data.subtitle;
-    document.getElementById("column1Image").src = data.column1Image.src;
-    document.getElementById("column1Image").alt = data.column1Image.alt;
     document.getElementById("column1Title").textContent = data.column1Title;
     document.getElementById("column1Text").textContent = data.column1Text;
-    document.getElementById("column2Image").src = data.column2Image.src;
-    document.getElementById("column2Image").alt = data.column2Image.alt;
     document.getElementById("column2Title").textContent = data.column2Title;
     document.getElementById("column2Text").textContent = data.column2Text;
-    document.getElementById("column3Image").src = data.column3Image.src;
-    document.getElementById("column3Image").alt = data.column3Image.alt;
     document.getElementById("column3Title").textContent = data.column3Title;
     document.getElementById("column3Text").textContent = data.column3Text;
   })
